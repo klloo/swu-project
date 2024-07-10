e9269cdbbbe786ff1c5fcb3184430dbae11f1e84
klloo <asdf016182@naver.com>
2024-07-11 02:20:55 +0900

    fix: 모바일 홈화면에 영상 적용

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,24 +6,36 @@ import Particle from '../components/Particle';
 function Home() {
   return (
     <>
-      <div className="h-full w-full absolute top-0 z-10 left-[50%] -translate-x-1/2">
-        <MagnifyText />
+      <div className="hidden lg:block">
+        <div className="h-full w-full absolute top-0 z-10 left-[50%] -translate-x-1/2">
+          <MagnifyText />
+        </div>
+        <div className="h-full w-full absolute top-0 z-5 left-[50%] -translate-x-1/2">
+          <Particle />
+        </div>
+        <div className="z-0">
+          <Canvas
+            camera={{ fov: 15, position: [0, 0, 5] }}
+            style={{
+              width: '100vw',
+              height: '100vh',
+              position: 'absolute',
+              top: 0,
+            }}
+          >
+            <Background />
+          </Canvas>
+        </div>
       </div>
-      <div className="h-full w-full absolute top-0 z-5 left-[50%] -translate-x-1/2">
-        <Particle />
-      </div>
-      <div className="z-0">
-        <Canvas
-          camera={{ fov: 15, position: [0, 0, 5] }}
-          style={{
-            width: '100vw',
-            height: '100vh',
-            position: 'absolute',
-            top: 0,
-          }}
-        >
-          <Background />
-        </Canvas>
+      <div className="lg:hidden w-[100vw] h-[100vh] absolute top-0 z-10">
+        <video
+          className="h-full w-full object-cover"
+          src="/video/main.mp4"
+          autoPlay
+          muted
+          loop
+          playsInline
+        />
       </div>
     </>
   );
