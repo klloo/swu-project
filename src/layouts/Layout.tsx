c29c50e02bc98d13b2070c1ec210a6b1c6783410
klloo <asdf016182@naver.com>
2024-07-11 15:33:58 +0900

    fix: 스크롤 초기화 되도록 수정

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 function MobileHeader({
@@ -76,6 +76,18 @@ function Layout() {
   }, [setShowMenu]);
   const navigate = useNavigate();
 
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  const scrollToTop = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollTop = 0;
+    }
+  };
+
+  useEffect(() => {
+    scrollToTop();
+  }, [location.pathname]);
+
   return (
     <>
       <div
@@ -85,88 +97,91 @@ function Layout() {
             : 'bg-[url("/images/pattern.svg")]'
         }`}
       >
-        <div className="relative min-h-[100vh]">
-          <header className="lg:flex hidden w-full justify-between sticky top-0 z-20 pt-7 px-10">
-            <Link to="/">
-              <img src="./images/nav-logo.svg" alt="main-logo" width="270" />
-            </Link>
-            {menuList.map((menu) => {
-              const isActive = location.pathname.includes(menu.link);
-              return (
-                <Link
-                  key={menu.link}
-                  to={menu.link}
-                  className={`text-2xl font-normal ${
-                    isActive ? 'underline underline-offset-[10px]' : ''
-                  }`}
-                >
-                  {menu.label}
-                </Link>
-              );
-            })}
-          </header>
-          <MobileHeader
-            onClick={onClickMobileMenu}
-            showMenu={showMenu}
-            setShowMenu={setShowMenu}
-          />
-          {showMenu && (
-            <div className="lg:hidden w-full fixed top-15 right-0 bg-[#BABCBE] z-50">
+        <div ref={scrollContainerRef} className="h-full w-full overflow-auto">
+          <div className="relative min-h-[100vh]">
+            <header className="lg:flex hidden w-full justify-between sticky top-0 z-20 pt-7 px-10">
+              <Link to="/">
+                <img src="./images/nav-logo.svg" alt="main-logo" width="270" />
+              </Link>
               {menuList.map((menu) => {
+                const isActive = location.pathname.includes(menu.link);
                 return (
-                  <div
+                  <Link
                     key={menu.link}
-                    onClick={() => {
-                      setShowMenu(false);
-                      navigate(menu.link);
-                    }}
-                    className={`text-2xl font-[18px] block px-5 pb-2 mt-5 border-b-[2px] border-black cursor-pointer`}
+                    to={menu.link}
+                    className={`text-2xl font-normal ${
+                      isActive ? 'underline underline-offset-[10px]' : ''
+                    }`}
                   >
                     {menu.label}
-                  </div>
+                  </Link>
                 );
               })}
+            </header>
+            <MobileHeader
+              onClick={onClickMobileMenu}
+              showMenu={showMenu}
+              setShowMenu={setShowMenu}
+            />
+            {showMenu && (
+              <div className="lg:hidden w-full fixed top-15 right-0 bg-[#BABCBE] z-50">
+                {menuList.map((menu) => {
+                  return (
+                    <div
+                      key={menu.link}
+                      onClick={() => {
+                        setShowMenu(false);
+                        navigate(menu.link);
+                      }}
+                      className={`text-2xl font-[18px] block px-5 pb-2 mt-5 border-b-[2px] border-black cursor-pointer`}
+                    >
+                      {menu.label}
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+            <div
+              id="content"
+              className={
+                !isHome ? 'relative z-10 mt-[50px] lg:px-7 px-5 pb-[26rem]' : ''
+              }
+            >
+              <Outlet />
             </div>
-          )}
-          <div
-            className={
-              !isHome ? 'relative z-10 mt-[50px] lg:px-7 px-5 pb-[26rem]' : ''
-            }
-          >
-            <Outlet />
+            {!isHome && (
+              <>
+                <footer className="lg:block hidden text-[18px] lg:px-7 px-5 pb-20 absolute bottom-0 w-full">
+                  <div className="relative">
+                    <img src="./images/logo.svg" alt="main-logo" width="380" />
+                    <div className="mt-4">
+                      2024 © Seoul Women’s University Visual Communication
+                      Design. All Rights Reserved.
+                    </div>
+                    <div className="flex mt-10 gap-24 leading-[27px]">
+                      <div>
+                        서울여자대학교
+                        <br /> 제41회 시각디자인전공 졸업전시회
+                      </div>
+                      <div>
+                        Seoul Women’s University
+                        <br />
+                        Visual Communication Design
+                        <br />
+                        Graduation Exhibition
+                      </div>
+                    </div>
+                    <img
+                      src="./images/swu-logo.svg"
+                      alt="swu-logo"
+                      className="absolute bottom-0 right-0"
+                    />
+                  </div>
+                </footer>
+                <MobileFooter />
+              </>
+            )}
           </div>
-          {!isHome && (
-            <>
-              <footer className="lg:block hidden text-[18px] lg:px-7 px-5 pb-20 absolute bottom-0 w-full">
-                <div className="relative">
-                  <img src="./images/logo.svg" alt="main-logo" width="380" />
-                  <div className="mt-4">
-                    2024 © Seoul Women’s University Visual Communication Design.
-                    All Rights Reserved.
-                  </div>
-                  <div className="flex mt-10 gap-24 leading-[27px]">
-                    <div>
-                      서울여자대학교
-                      <br /> 제41회 시각디자인전공 졸업전시회
-                    </div>
-                    <div>
-                      Seoul Women’s University
-                      <br />
-                      Visual Communication Design
-                      <br />
-                      Graduation Exhibition
-                    </div>
-                  </div>
-                  <img
-                    src="./images/swu-logo.svg"
-                    alt="swu-logo"
-                    className="absolute bottom-0 right-0"
-                  />
-                </div>
-              </footer>
-              <MobileFooter />
-            </>
-          )}
         </div>
       </div>
     </>
